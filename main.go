2b783f40537ff7087f927edd26609a8e32acdca0
Yassine Najmi <yassine.najmi01@gmail.com>
2026-08-26 19:43:32 +0100

    feat: add the job and the in memory store struct

--- main.go
+++ main.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Job struct {
+	ID          int            `json:"id"`
+	Type        string         `json:"type"`
+	Payload     map[string]any `json:"payload"`
+	Status      string         `json:"status"` // queued, running, retrying, success, failed
+	Attempts    int            `json:"attempts"`
+	MaxAttempts int            `json:"max_attempts"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+}
+
+type InMemoryStore struct {
+	jobs map[int]Job
+}
+
+func NewInMemoryStore() *InMemoryStore {
+	return &InMemoryStore{
+		jobs: make(map[int]Job),
+	}
+}
 
 func main() {
 	fmt.Println("Job Queue - work in progress")
